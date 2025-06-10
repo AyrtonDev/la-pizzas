7a34f0804aee4e736d543aa53e7a48d01d5377fb
Luan Cruz <luanncruzz20@gmail.com>
2025-06-10 18:00:35 -0400

    feat: button homepage

--- src/modules/home-module/page/home.tsx
+++ src/modules/home-module/page/home.tsx
@@ -2,10 +2,11 @@ import { Button } from '@/shared/components/ui/button';
 
 export default function HomePage() {
   return (
-    <div className="relative w-screen h-[90vh] flex justify-center items-center bg-[#F2EEE8] overflow-hidden">
+    <div className="relative w-screen h-[90vh] flex justify-center items-center bg-light overflow-hidden">
       <img src="./public/bgHome.jpg" alt="imagem de fundo" className="w-full h-full object-cover" />
-      <div className="absolute inset-0 bg-orange-900/50">
-        <Button>Button</Button>
+      <div className="w-full h-full absolute inset-0 bg-black/70 flex justify-center items-center flex-col gap-20">
+        <h1 className=' font-bold text-light font-serif text-2xl text-center'>O sabor da Itália direto pra sua mesa</h1>
+        <Button className='text-light font-display text-lg p-5 shadow-lg/50 cursor-pointer'>Ver Cardápio</Button>
       </div>
     </div>
   );
