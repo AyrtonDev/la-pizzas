ecf7b27a7565a3629fd0077ff518344107092383
Luan Cruz <luanncruzz20@gmail.com>
2025-06-10 16:49:06 -0400

    feat: create header, homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import Header from './shared/components/header/header';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/modules/home-module/page/home.tsx
+++ src/modules/home-module/page/home.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/shared/components/ui/button';
+
 export default function HomePage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="text-4xl ">Página Home</h1>
+    <div className="relative w-screen h-[90vh] flex justify-center items-center bg-[#F2EEE8] overflow-hidden">
+      <img src="./public/bgHome.jpg" alt="imagem de fundo" className="w-full h-full object-cover" />
+      <div className="absolute inset-0 bg-orange-900/50">
+        <Button>Button</Button>
+      </div>
     </div>
   );
 }
