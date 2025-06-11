f213b7299936d17f73c083c9bad280f3908f3abb
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-06-11 12:43:35 -0300

    feat: json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
+
 import { router } from './routes';
 import Header from './shared/components/header/header';
 
--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -2,7 +2,7 @@ import logo from '/public/logoHorizontal.svg';
 
 export default function Header() {
   return (
-    <div className="w-screen h-[10vh] flex pl-5 bg-[#fdb94c]">
+    <div className="flex h-[10vh] w-screen bg-[#fdb94c] pl-5">
       <img src={logo} alt="logo" className="w-25" />
     </div>
   );
