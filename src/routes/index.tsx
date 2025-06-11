35c5dc484cf74600bafa17fba1a51bc80c19a874
Luan Cruz <luanncruzz20@gmail.com>
2025-06-11 11:45:57 -0400

    feat: initial menu page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '@/modules/home-module/page/home';
+import MenuPage from '@/modules/menu-module/page/menu';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/cardapio',
+    element: <MenuPage/>
+  }
 ]);
