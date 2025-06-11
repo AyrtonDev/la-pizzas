f13478f0fa963df0619f38da98a3d54294d1351b
Luan Cruz <luanncruzz20@gmail.com>
2025-06-11 11:18:00 -0400

    feat: responsive home page

--- src/modules/home-module/page/home.tsx
+++ src/modules/home-module/page/home.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div className="relative w-screen h-[90vh] flex justify-center items-center bg-light overflow-hidden">
       <img src="./public/bgHome.jpg" alt="imagem de fundo" className="w-full h-full object-cover" />
       <div className="w-full h-full absolute inset-0 bg-black/70 flex justify-center items-center flex-col gap-20">
-        <h1 className=' font-bold text-light font-serif text-2xl text-center'>O sabor da Itália direto pra sua mesa</h1>
+        <h1 className=' font-bold text-light font-serif sm:text-4xl text-lg text-center'>O sabor da Itália direto pra sua mesa</h1>
         <Button className='text-light font-display text-lg p-5 shadow-lg/50 cursor-pointer'>Ver Cardápio</Button>
       </div>
     </div>
--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -2,7 +2,7 @@ import logo from '/public/logoHorizontal.svg';
 
 export default function Header() {
   return (
-    <div className="w-screen h-[10vh] flex pl-5 bg-[#fdb94c] ">
+    <div className="w-screen h-[10vh] flex pl-5 bg-[#fdb94c]">
       <img src={logo} alt="logo" className="w-25" />
     </div>
   );
