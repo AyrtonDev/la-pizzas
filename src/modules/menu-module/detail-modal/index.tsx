d88ab4f3805fa4e68bf2493cad7e039a47ca5055
Luan Cruz <luanncruzz20@gmail.com>
2025-06-13 15:37:34 -0400

    feat: responsive layout modal and menu

--- src/modules/menu-module/detail-modal/index.tsx
+++ src/modules/menu-module/detail-modal/index.tsx
@@ -43,13 +43,13 @@ export const DetailModal = ({ children, pizza }: DetailModalProps) => {
       <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="bg-light sm:max-w-[425px]">
         <div className="flex h-full w-full flex-col gap-4">
-          <div className="flex w-full justify-between">
+          <div className="flex w-full flex-col items-center justify-between sm:flex-row sm:items-start">
             <img
               src={`/images/pizzas/${pizza.image}`}
               alt="pizza de calabresa"
               className="size-50"
             />
-            <div className="flex flex-col gap-18">
+            <div className="flex flex-col gap-4 sm:gap-18">
               <h1 className="text-primary text-3xl font-bold">{pizza.name}</h1>
               <div className="flex flex-col items-center gap-2">
                 <span className="text-xl font-bold">Quantidade</span>
@@ -75,10 +75,13 @@ export const DetailModal = ({ children, pizza }: DetailModalProps) => {
           </div>
           <p className="text-justify text-xl">{pizza.description}</p>
           <div className="flex items-center justify-around">
-            <Button className="text-md h-10 w-1/2 font-bold text-white" onClick={handleAddToCart}>
+            <Button
+              className="sm:text-md h-10 font-bold text-white sm:w-1/2"
+              onClick={handleAddToCart}
+            >
               Adicionar a Carrinho
             </Button>
-            <span className="text-primary text-4xl font-bold">
+            <span className="text-primary text-xl font-bold sm:text-4xl">
               {formatCurrency(pizza.price * count)}
             </span>
           </div>
